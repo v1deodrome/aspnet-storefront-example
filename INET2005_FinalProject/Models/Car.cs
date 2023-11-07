de2b7a01cabafc4fb4e4a93e59768cd6d4abc792
jeverill0462954 <w0462954@campus.nscc.ca>
2023-11-07 11:39:38 -0400

    Type select list + added ImageName attribute

--- INET2005_FinalProject/Models/Car.cs
+++ INET2005_FinalProject/Models/Car.cs
@@ -8,6 +8,7 @@
         public int ModelYear { get; set; }
         public string Color { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string ImageName {  get; set; } = string.Empty;
 
         // Foreign key
         public required CarType CarType { get; set; }
--- INET2005_FinalProject/Pages/CarPages/Create.cshtml.cs
+++ INET2005_FinalProject/Pages/CarPages/Create.cshtml.cs
@@ -7,16 +7,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using INET2005_FinalProject.Data;
 using INET2005_FinalProject.Models;
+using System.ComponentModel;
 
 namespace INET2005_FinalProject.Pages.CarPages
 {
     public class CreateModel : PageModel
     {
         private readonly INET2005_FinalProject.Data.INET2005_FinalProjectContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public CreateModel(INET2005_FinalProject.Data.INET2005_FinalProjectContext context)
+        // List of car types
+        public List<SelectListItem> CarTypeList { get; set; } = new List<SelectListItem>();
+
+        public CreateModel(INET2005_FinalProject.Data.INET2005_FinalProjectContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
+
+            // Populating the type list 
+            List<CarType> carTypes = _context.CarType.ToList();
+
+            foreach (CarType carType in carTypes)
+            {
+                CarTypeList.Add(new SelectListItem()
+                {
+                    Value = carType.CarTypeID.ToString(),
+                    Text = carType.TypeName
+                });
+            }
         }
 
         public IActionResult OnGet()
@@ -31,7 +49,13 @@ namespace INET2005_FinalProject.Pages.CarPages
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Car == null || Car == null)
+            // Get and set TypeID
+            int typeID = Car.CarType.CarTypeID;
+            CarType carType = _context.CarType.Single(m => m.CarTypeID == typeID);
+            Car.CarType = carType;
+
+            // Validate
+            if (!ModelState.IsValid || _context.Car == null || Car == null)
             {
                 return Page();
             }
