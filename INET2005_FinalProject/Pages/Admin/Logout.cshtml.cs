8a3fdebcf778c29927b1718e7f3042885c2889de
jeverill0462954 <w0462954@campus.nscc.ca>
2023-11-09 10:09:30 -0400

    Cookie authentication in Program.cs

---
+++ INET2005_FinalProject/Pages/Admin/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace INET2005_FinalProject.Pages.Admin
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- INET2005_FinalProject/Program.cs
+++ INET2005_FinalProject/Program.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using INET2005_FinalProject.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<INET2005_FinalProjectContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("INET2005_FinalProjectContext") ?? throw new InvalidOperationException("Connection string 'INET2005_FinalProjectContext' not found.")));
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddCookie(options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    options.SlidingExpiration = true;
+    options.LoginPath = "/Admin/Login";
+    options.LogoutPath = "/Admin/Logout";
+});
 
 var app = builder.Build();
 
