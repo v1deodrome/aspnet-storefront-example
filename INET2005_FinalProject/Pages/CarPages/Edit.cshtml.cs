26beef3073f62008358f81f47215173ab3306075
jeverill0462954 <w0462954@campus.nscc.ca>
2023-11-15 10:51:09 -0400

    Fixed edit page (still can't edit images)

--- INET2005_FinalProject/Pages/CarPages/Edit.cshtml.cs
+++ INET2005_FinalProject/Pages/CarPages/Edit.cshtml.cs
@@ -16,14 +16,35 @@ namespace INET2005_FinalProject.Pages.CarPages
     public class EditModel : PageModel
     {
         private readonly INET2005_FinalProject.Data.INET2005_FinalProjectContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public EditModel(INET2005_FinalProject.Data.INET2005_FinalProjectContext context)
+        // Select list
+        public List<SelectListItem> CarTypeList { get; set; } = new List<SelectListItem>();
+
+        public EditModel(INET2005_FinalProject.Data.INET2005_FinalProjectContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
+
+            // Populating the type list 
+            List<CarType> carTypes = _context.CarType.ToList();
+
+            foreach (CarType carType in carTypes)
+            {
+                CarTypeList.Add(new SelectListItem()
+                {
+                    Value = carType.CarTypeID.ToString(),
+                    Text = carType.TypeName
+                });
+            }
+            _env = env;
         }
 
         [BindProperty]
         public Car Car { get; set; } = default!;
+        
+        // [BindProperty]
+        // public IFormFile ImageUpload { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -32,19 +53,29 @@ namespace INET2005_FinalProject.Pages.CarPages
                 return NotFound();
             }
 
-            var car =  await _context.Car.FirstOrDefaultAsync(m => m.CarID == id);
+            var car = await _context.Car.Include("CarType").FirstOrDefaultAsync(m => m.CarID == id);
             if (car == null)
             {
                 return NotFound();
             }
+            
             Car = car;
+
             return Page();
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
+            // string imageFileName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss_") + ImageUpload.FileName;
+            // Car.ImageName = imageFileName;
+
+            // Get and set TypeID
+            int typeID = Car.CarType.CarTypeID;
+            CarType carType = _context.CarType.Single(m => m.CarTypeID == typeID);
+            Car.CarType = carType;
+
             if (!ModelState.IsValid)
             {
                 return Page();
@@ -55,6 +86,16 @@ namespace INET2005_FinalProject.Pages.CarPages
             try
             {
                 await _context.SaveChangesAsync();
+
+                //if (ImageUpload != null)
+                //{
+                //    // Save image to filesystem
+                //    string filePath = Path.Combine(_env.ContentRootPath, "wwwroot/photos", imageFileName);
+                //    using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                //    {
+                //        ImageUpload.CopyTo(fileStream);
+                //    }
+                //}
             }
             catch (DbUpdateConcurrencyException)
             {
